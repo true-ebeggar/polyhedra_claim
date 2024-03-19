80540d090dcecbd4b2b60a0f83bfa7aad24fe6b8
Reilighost <reilighost@gmail.com>
2024-03-19 23:13:06 +0100

    fix fix

--- main.py
+++ main.py
@@ -61,25 +61,25 @@ def process_keys():
         try:
             with open('success_keys.txt', 'a') as file:
                 for key in success_keys:
-                    if key not in file:
-                        file.write(key + '\n')
+                    file.write(key + '\n')
+
         except FileNotFoundError:
             logger.warning("File 'success_keys.txt' not found. Creating a new file.")
             with open('success_keys.txt', 'w') as file:
                 for key in success_keys:
-                    if key not in file:
-                        file.write(key + '\n')
+                    file.write(key + '\n')
+
         try:
             with open('fail_keys.txt', 'a') as file:
                 for key in fail_keys:
-                    if key not in file:
-                        file.write(key + '\n')
+                    file.write(key + '\n')
+
         except FileNotFoundError:
             logger.warning("File 'fail_keys.txt' not found. Creating a new file.")
             with open('fail_keys.txt', 'w') as file:
                 for key in fail_keys:
-                    if key not in file:
-                        file.write(key + '\n')
+                    file.write(key + '\n')
+
         t = random.randint(sleep_min, sleep_max)
         logger.info(f"Process will hold for {t}-sec")
         time.sleep(t)
