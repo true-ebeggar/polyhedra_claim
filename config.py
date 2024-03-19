1ee9962560909c06a41d5b8668e35a800925b720
Reilighost <reilighost@gmail.com>
2024-03-19 22:26:26 +0100

    tested version

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 
 sleep_min, sleep_max = 60, 120
 gas_lim_min, gas_lim_max = 85000, 100000
-MAX_GAS = 33
+MAX_GAS = 40
 NODE = 'https://eth.llamarpc.com'
 SHUFLE = True
--- main.py
+++ main.py
@@ -51,33 +51,33 @@ def process_keys():
             success_keys.append(key)
         else:
             fail_keys.append(key)
+        with open('private_keys.txt', 'w') as file:
+            for key in private_keys:
+                if key not in success_keys and key not in fail_keys:
+                    file.write(key + '\n')
 
-    with open('private_keys.txt', 'w') as file:
-        for key in private_keys:
-            if key not in success_keys and key not in fail_keys:
-                file.write(key + '\n')
+        try:
+            with open('success_keys.txt', 'a') as file:
+                for key in success_keys:
+                    file.write(key + '\n')
+        except FileNotFoundError:
+            logger.warning("File 'success_keys.txt' not found. Creating a new file.")
+            with open('success_keys.txt', 'w') as file:
+                for key in success_keys:
+                    file.write(key + '\n')
 
-    try:
-        with open('success_keys.txt', 'a') as file:
-            for key in success_keys:
-                file.write(key + '\n')
-    except FileNotFoundError:
-        logger.warning("File 'success_keys.txt' not found. Creating a new file.")
-        with open('success_keys.txt', 'w') as file:
-            for key in success_keys:
-                file.write(key + '\n')
-
-    try:
-        with open('fail_keys.txt', 'a') as file:
-            for key in fail_keys:
-                file.write(key + '\n')
-    except FileNotFoundError:
-        logger.warning("File 'fail_keys.txt' not found. Creating a new file.")
-        with open('fail_keys.txt', 'w') as file:
-            for key in fail_keys:
-                file.write(key + '\n')
-
+        try:
+            with open('fail_keys.txt', 'a') as file:
+                for key in fail_keys:
+                    file.write(key + '\n')
+        except FileNotFoundError:
+            logger.warning("File 'fail_keys.txt' not found. Creating a new file.")
+            with open('fail_keys.txt', 'w') as file:
+                for key in fail_keys:
+                    file.write(key + '\n')
+        t = random.randint(sleep_min, sleep_max)
+        logger.info(f"Process will hold for {t}-sec")
+        time.sleep(t)
 
 if __name__ == "__main__":
     process_keys()
-    
